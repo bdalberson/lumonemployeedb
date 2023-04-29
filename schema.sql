3a8bb094726608c5dd5979f7c3880ba1d323de7d
bdalberson <Brian.alberson@gmail.com>
2023-04-29 12:59:11 -0700

    added some dependencies, readme and got my SQL table populated correctly.

--- schema.sql
+++ schema.sql
@@ -5,38 +5,67 @@ USE lumon_db;
 
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT ,
+    id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL
+    salary DECIMAL,
     FOREIGN KEY department(id)
-    REFERENCES department(id),
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY role_id(id)
-  REFERENCES role(id),
-  manager_id INT NOT NULL AUTO_INCREMENT
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+    REFERENCES role(id),
+  FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
 
 
+INSERT INTO department (name)
+VALUES ("Macrodata Refinement"),
+       ("Optics and Design"),
+       ("Wellness Director"),
+       ("Security"),
+       ("CEO");
 
+SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO role(title, salary)
+VALUES ("Refiner", 120000),
+    ("Manager", 140000),
+    ("Designer", 120000),
+    ("Therapist", 160000),
+    ("Guard", 100000),
+    ("CEO", 500000);
+       
 
-
-
-INSERT INTO movies_names (name)
-VALUES ("Super Mario"),
-       ("Encanto"),
-       ("Little Mermaid");
-
-INSERT INTO movies_reviews (review)
-VALUES ("This was great"),
-       ("Loved the songs"),
-       ("Daughter loved it");
+SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES 
+    ("Mark", "S", 1, 5),
+    ("Helly", "R", 1, 5),
+    ("Dylan", "G", 1, 5),
+    ("Doug", "Grander", 4, 7),
+    ("Seth", "Milchick", 1, 7),
+    ("Judd", "Z", 5, 4),
+    ("Harmony", "Cobel", 2, 17),
+    ("Irving", "Bailiff", 1, 7),
+    ("Burt", "Goodman", 2, 5),
+    ("Gemma", "Casey", 3, 7),
+    ("Natalie", "A", 1, 17),
+    ("Peter", "Kilmer", 1, 5),
+    ("Kier", "Eagan", 5, 17),
+    ("Ambrose", "Eagan", 5, 17),
+    ("Baird", "Eagan", 5, 17),
+    ("Leonora", "Eagan", 5, 17),
+    ("Phillip", "Eagan", 5, 17),
+    ("Jame", "Eagan", 5, 17);
